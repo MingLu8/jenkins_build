44fa4af351302f3a3daa19babdd6038b1aa47e0e
MingLu8 <ming.c.lu@gmail.com>
2017-10-14 01:31:42 -0400

    initial check in.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+set -e
+dotnet restore
+dotnet build
+# dotnet test test/WebTests/project.json =
+
+rm -rf $(pwd)/out
+
+dotnet publish aspnetcore/aspnetcore.csproj -c Release -o $(pwd)/out
+cp Dockerfile $(pwd)/out  
