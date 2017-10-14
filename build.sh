f6047a28c207869d032914c8af7aaddf9f73e2cc
MingLu8 <ming.c.lu@gmail.com>
2017-10-14 02:36:48 -0400

    UNIX.

--- build.sh
+++ build.sh
@@ -2,19 +2,9 @@
 set -e
 dotnet restore
 dotnet build
-<<<<<<< HEAD
-# dotnet test test/WebTests/project.json =
-=======
-# dotnet test test/WebTests/project.json
->>>>>>> 996d905d093d0b0459ef42ce7cfa60d411807121
 
 rm -rf $(pwd)/out
 
 dotnet publish aspnetcore/aspnetcore.csproj -c Release -o $(pwd)/out
-<<<<<<< HEAD
-
 
 cp Dockerfile $(pwd)/out  
-=======
-cp Dockerfile $(pwd)/out
->>>>>>> 996d905d093d0b0459ef42ce7cfa60d411807121
